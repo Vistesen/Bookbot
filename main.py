33088ee259e496fd8e87de29785f29d8b4184f11
Vistesen <Martin_8700@live.dk>
2025-09-17 18:57:47 +0200

    i hope this works

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-    #print(text)
 
 
 main()
