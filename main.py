eaa53b5b844b79ae93b93703f5ebf20909c06ca3
Vistesen <Martin_8700@live.dk>
2025-09-17 19:13:34 +0200

    small progress on Bookbot project

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from stats import get_num_words
+from stats import get_num_words, char_frequency
 
 def get_book_text(file_path):
     with open(file_path) as f:
-        return f.read() 
+        return f.read()
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+
+if __name__ == "__main__":
+    main()q = char_frequency(text)
+    #print(text)
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,9 @@ def get_num_words(some_text):
     parts = some_text.split()
     return len(parts)
 
+def char_frequency(some_text):
+    freq = {}
+    for char in some_text:
+        char = char.lower()
+        freq[char] = freq.get(char, 0) + 1
+    return freq
