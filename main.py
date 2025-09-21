0e2053704e21e45f3f8bf066f346d98fd0e664ee
Vistesen <Martin_8700@live.dk>
2025-09-21 19:08:17 +0200

    new changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, char_frequency, sort_char_counts
 
 def get_book_text(file_path):
@@ -5,14 +6,18 @@ def get_book_text(file_path):
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    text = get_book_text(file_path)
     num_words = get_num_words(text)
     # Call your new function and store the result
     char_counts = char_frequency(text)
     sorted_chars = sort_char_counts(char_counts)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     # Print the character counts dictionary as per assignment
